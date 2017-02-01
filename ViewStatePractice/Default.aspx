015d046732c356c2386038742f98053f4d78ba2a
Juyoung Choi <juy@outlook.dk>
2017-02-01 14:29:26 +0100

    initial commit

---
+++ ViewStatePractice/Default.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ViewStatePractice.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            You clicked  : 
+        <br />
+            <asp:Label ID="counterLabel" runat="server" />
+            <br />
+
+            <asp:Button ID="countButton" runat="server" Text="Count" OnClick="countButton_Click" />
+            <asp:Button ID="clearButton" runat="server" Text="Clear" OnClick="clearButton_Click" />
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ ViewStatePractice/Default.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ViewStatePractice {
+    public partial class Default : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+
+        }
+
+        protected void countButton_Click(object sender, EventArgs e) {
+            int counter;
+            if (ViewState["counter"] == null) {
+                counter = 1;
+            } else {
+                counter = (int)ViewState["counter"] + 1;
+            }
+            ViewState["counter"] = counter;
+            counterLabel.Text = counter.ToString();
+        }
+
+        protected void clearButton_Click(object sender, EventArgs e) {
+            if (ViewState["counter"] != null)
+                ViewState.Remove("counter");
+            counterLabel.Text = string.Empty;
+
+        }
+    }
+}
